a4e3ba8eec4f3572a10eada4e389449a09b86ccc
Logeshwaran <logeshwaran2982.com>
2025-11-18 14:38:54 +0530

    to create a server

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,1 +1,27 @@
-const app = require('./app')
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+
+
+app.use(express.json());
+
+// connect to mongodb
+
+mongoose.connect(process.env.DB_CONNECTION_STRING,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(()=>{
+    console.log('Database is Connected Sucessfully')
+})
+.catch((err)=>{
+    console.log('MongoDb Connection Error',err)
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(port,()=>{
+    console.log(`Server is running on ${port}`);
+    
+})
