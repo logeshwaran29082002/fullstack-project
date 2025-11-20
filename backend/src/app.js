9ffae2869655ff5b6f7814f858c612c7c422d0fd
Logeshwaran <logurashitha2002@gmail.com>
2025-11-20 12:17:37 +0530

    cors  install

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const app = express();
 const router = require("./routes/userRoutes");
+const cors = require('cors')
 require("dotenv").config();
+
+
+app.use(cors()); // Enable cors for all route
 
 app.use(express.json());
 
