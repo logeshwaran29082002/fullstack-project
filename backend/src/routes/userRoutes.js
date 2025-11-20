352bd91e99254b8f6f494d67a6ca2cc02dca2473
Logeshwaran <logurashitha2002@gmail.com>
2025-11-20 16:32:46 +0530

    checking

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -13,8 +13,8 @@ const { verifyToken } = require("../middleware/auth");
 
 
 // signup
-router.post("/signup", signup);
-router.post("/verify-otp", verifyOTP);
+router.post("/signup", signup);  // api  http://localhost:5000/api/signup
+router.post("/verify-otp", verifyOTP);  // api  http://localhost:5000/api/verify-otp
 router.post("/resend-otp", resendOTP);
 
 
